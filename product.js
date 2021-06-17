6fd05b04f2ed87791bced30b46dd9e1aaaa7bacf
Jafar Al-Juneidi <aljuneidi.jafar@gmail.com>
2021-06-17 17:07:27 +0300

    product page

---
+++ product.js
@@ -0,0 +1,24 @@
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const number = document.getElementById('number');
+const heart = document.getElementById('heart');
+
+plus.addEventListener('click', () => {
+    number.innerHTML = parseInt(number.innerHTML) + 1;
+});
+
+minus.addEventListener('click', () => {
+    if (parseInt(number.innerHTML) > 0) {
+        number.innerHTML = parseInt(number.innerHTML) - 1;
+    }
+});
+
+heart.addEventListener('click', () => {
+    console.log(heart.style);
+    const color = heart.style.color;
+    if (color != 'red') {
+        heart.style.color = 'red';
+    } else if (color == 'red') {
+        heart.style.color = '';
+    }
+});
